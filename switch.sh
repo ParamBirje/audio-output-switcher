35909111d7665e1cfcee5ae8a7bbf1e7636b3041
Param Birje <parambirje@gmail.com>
2024-01-31 19:20:30 +0530

    first commit

---
+++ switch.sh
@@ -0,0 +1,11 @@
+choice=$1
+
+if [ -z $choice ] || [ $choice -eq 0 ]; then
+	# Monitor
+	pactl set-default-sink alsa_output.pci-0000_06_00.1.hdmi-stereo
+else
+	# Headphone
+	pactl set-default-sink alsa_output.pci-0000_06_00.6.analog-stereo
+fi
+
+exit
